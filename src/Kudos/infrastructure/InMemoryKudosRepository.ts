7d9d4562aa9c51e6960a323c44db48aec7c0bb7d
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2020-10-25 19:42:12 +0100

    add fisrt types

--- src/Kudos/infrastructure/InMemoryKudosRepository.ts
+++ src/Kudos/infrastructure/InMemoryKudosRepository.ts
@@ -1,13 +1,18 @@
-export default class InMemoryKudosRepository {
-  constructor({ kudos = [] } = {}) {
+import KudosRepository from "../models/KudosRepository";
+import Kudos from "../models/Kudos";
+
+export default class InMemoryKudosRepository implements KudosRepository {
+  _kudos: Map<string,Kudos>;
+
+  constructor({ kudos = new Map() } = {}) {
     this._kudos = kudos;
   }
 
-  save(kudos) {
-    this._kudos[kudos.recipient()] = kudos;
+  save(kudos: Kudos): void {
+    this._kudos.set(kudos.recipient(), kudos);
   }
 
-  search({ recipient }) {
-    return this._kudos[recipient];
+  search({ recipient }: { recipient: string }): Kudos | undefined {
+    return this._kudos.get(recipient);
   }
 }
--- src/Kudos/models/KudosRepository.ts
+++ src/Kudos/models/KudosRepository.ts
@@ -1,1 +1,9 @@
-// @todo with typescript, define here the interface for KudosRepository
+import Kudos from "./Kudos";
+
+interface KudosRepository {
+  save(kudos: Kudos): void;
+  // @to-do define a type criteria
+  search(criteria: { recipient: string }): Kudos | undefined;
+}
+
+export default KudosRepository;
