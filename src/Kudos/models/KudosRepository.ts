7a5be102d2e2c76ff0587081884be7ec831cc1fc
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2020-10-25 19:48:18 +0100

    add a todo for criteria type

--- src/Kudos/models/KudosRepository.ts
+++ src/Kudos/models/KudosRepository.ts
@@ -2,7 +2,7 @@ import Kudos from "./Kudos";
 
 interface KudosRepository {
   save(kudos: Kudos): void;
-  // @to-do define a type criteria
+  // @todo define a type criteria
   search(criteria: { recipient: string }): Kudos | undefined;
 }
 
